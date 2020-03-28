3dbb873e5f3890d6933c6ed62133c1aa52d0fa33
milena-mognon <milenamognon@gmail.com>
2020-03-28 17:08:32 -0300

    incidents pagination

--- backend/src/app/controllers/IncidentsController.js
+++ backend/src/app/controllers/IncidentsController.js
@@ -2,8 +2,16 @@ import { connection } from '../../database';
 
 class IncidentsController {
   async index(req, res) {
-    const incidents = await connection('incidents').select('*');
+    const { page = 1 } = req.query;
 
+    const [count] = await connection('incidents').count();
+
+    const incidents = await connection('incidents')
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select('*');
+
+    res.header('X-Total-Count', count['count(*)']);
     return res.json(incidents);
   }
 
