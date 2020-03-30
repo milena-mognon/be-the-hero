18411a5005b9912326b03d1dee42821296018298
milena-mognon <milenamognon@gmail.com>
2020-03-30 14:28:02 -0300

    Finished profile styles

--- frontend/src/pages/components/Profile/index.js
+++ frontend/src/pages/components/Profile/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPower } from 'react-icons/fi';
-import { Container } from './styles';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+import { Container, List } from './styles';
 import logoImg from '../../../assets/images/logo.svg';
 
 export default function Profile() {
@@ -18,6 +18,70 @@ export default function Profile() {
           <FiPower size={18} color="#e02041" />
         </button>
       </header>
+
+      <h1>Casos cadastrados</h1>
+
+      <List>
+        <li>
+          <strong>CASOS: </strong>
+          <p>Casos Teste</p>
+
+          <strong>DESCRIÇÃO: </strong>
+          <p>Descrição Teste</p>
+
+          <strong>VALOR: </strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASOS: </strong>
+          <p>Casos Teste</p>
+
+          <strong>DESCRIÇÃO: </strong>
+          <p>Descrição Teste</p>
+
+          <strong>VALOR: </strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASOS: </strong>
+          <p>Casos Teste</p>
+
+          <strong>DESCRIÇÃO: </strong>
+          <p>Descrição Teste</p>
+
+          <strong>VALOR: </strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>CASOS: </strong>
+          <p>Casos Teste</p>
+
+          <strong>DESCRIÇÃO: </strong>
+          <p>Descrição Teste</p>
+
+          <strong>VALOR: </strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+      </List>
     </Container>
   );
 }
--- frontend/src/pages/components/Profile/styles.js
+++ frontend/src/pages/components/Profile/styles.js
@@ -55,4 +55,51 @@ export const Container = styled.div`
       }
     }
   }
+
+  h1 {
+    margin-top: 80px;
+    margin-bottom: 24px;
+  }
+`;
+
+export const List = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 24px;
+  list-style: none;
+
+  li {
+    background: #fff;
+    padding: 24px;
+    border-radius: 8px;
+    position: relative;
+
+    button {
+      position: absolute;
+      right: 24px;
+      top: 24px;
+      border: 0;
+      background: transparent;
+
+      &:hover {
+        opacity: 0.8;
+      }
+    }
+
+    strong {
+      display: block;
+      margin-bottom: 16px;
+      color: #41414d;
+    }
+
+    p + strong {
+      margin-top: 32px;
+    }
+
+    p {
+      color: #737380;
+      line-height: 21px;
+      font-size: 16px;
+    }
+  }
 `;
